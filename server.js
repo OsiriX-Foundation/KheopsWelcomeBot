6b4f0aa50a74f06c37547c0268126316eed78d8c
Joël Spaltenstein <spalte@naturalimage.ch>
2019-02-21 21:53:49 +0100

    cleanup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = 8080;
